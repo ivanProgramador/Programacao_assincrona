a57aef30f7e6884a1e26592f0a528206d96329ab
ivan <ivan-guita@hotmail.com>
2023-05-31 19:16:24 -0300

    promise

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-// Processos assincronos no codigo abaixo eu vou simulkar um sistema de envio de e-mail,
-//a afunção abaixo recebe 2 parametros o destianatario do email eo corpo do e-mail dentro dela
-// estou usando O setTimeout para defginir que ela vai ser executada em 3 segundos so que junto com essa 
-//funcção eu vou colocar mais 3 processos que vão ser executados de forma assincrona o console vai mostrar claramente 
-// que o node executa as funções mais rapidas en quanto a função de enviar email espera os 3 segundos 
-//emabora exsita uma sequecia de execução ela é ignorada pelo porcessamento assincrono 
 
-console.log('enviando e-mail')
+//essa eo uso de uma promisse pra checar se ela deu certo ou errado  
 
 function enviarEmail(corpo,para){
+     return new Promise((resolve,reject)=>{
+         var deuErro = true;
 
-     setTimeout(()=>{
-        console.log(
-            `Para: ${para}
-             --------------------------------
-             ${corpo}
-             --------------------------------------
-             De> Victor Lima
-            `
-        );
-     },3000)
+         if(!deuErro){
+             resolve()
+         }else{
+            reject()
+         }
+     }, 4000)
+
 }
 
-enviarEmail('seja bem vindo ','Pedro')
+enviarEmail("Olá mundo","ivan@udemy.com.br").then(()=>{
 
-console.log('e-mail enviado');
-console.log('seu e-mail chegou');
+    var a = 1+1;
+    console.log("Opa voce conseguiu fazer oque prometeu");
+    console.log(a);
+
+}).catch(()=>{
+
+    console.log("deu erro ")
+})
